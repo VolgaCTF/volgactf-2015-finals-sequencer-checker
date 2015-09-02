4a094b8e3083d65cc6822a5d1760c5af0b439c5a
volalex <vol.alexy@gmail.com>
2015-09-02 20:16:20 +0400

    Initial commit

---
+++ checker.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+__author__ = 'alexey'
+from themis.checker import Server, Result
+import string, random, os
+import requests
+import tempfile
+
+class SequencerChecker(Server):
+
+    functions = ["count-nucleo", "dna-to-rna", "reverse-complement-dna"]
+
+    def create_task_string(self,flag):
+        task = ""
+        task+="name;"+self.genlogin()+"\n"
+        task+="func;"+random.choice(self.functions)+"\n"
+        data = ''.join(random.choice(['A', 'C', "T", "G"]) for i in range(random.randint(5,15)))
+        task+="data;"+"{"+"s1 "+data+"}"+"\n"
+        task+="comment;"+flag+"\n"
+        return task
+
+    def create_temp_task_file(self,flag):
+        fd, filename = tempfile.mkstemp("sequencer",text=True)
+        os.write(fd, self.create_task_string(flag))
+        os.close(fd)
+        return fd,filename
+
+    def genpassword(self):
+        length = random.randint(10, 15)
+        chars = string.ascii_letters + string.digits + '_'
+        random.seed = (os.urandom(1024))
+        return ''.join(random.choice(chars) for i in range(length))
+
+    def genlogin(self):
+        length = random.randint(5,12)
+        chars = string.ascii_letters+string.digits+'_'
+        random.seed = (os.urandom(1024))
+        return ''.join(random.choice(chars) for i in range(length))
+
+    def push(self, endpoint, flag_id, flag):
+        username = self.genlogin()
+        password = self.genpassword()
+        payload = {'username':username, 'password':password}
+        try:
+            r = requests.post("http://"+endpoint+":8080/do-register", data=payload, timeout=10)
+            if r.status_code == 200:
+                try:
+                    fd, filename = checker.create_temp_task_file(flag)
+                    cookie = r.history[0].cookies["JSESSIONID"]
+                    files = {'task': open(filename,"r")}
+                    r2 = requests.post("http://"+endpoint+":8080/",files=files)
+                    if r2.status_code == 200:
+                        text = r2.text
+                        if not flag in text:
+                            return Result.CORRUPT
+                        else:
+                            return Result.OK, username+";"+password
+                finally:
+                    os.remove(fd)
+            else:
+                return Result.MUMBLE
+        except requests.ConnectionError:
+            return Result.DOWN
+        except requests.HTTPError:
+            return Result.DOWN
+        except requests.exceptions.Timeout:
+            return Result.DOWN
+
+    def pull(self, endpoint, flag_id, flag):
+        pass
+
+
+checker = SequencerChecker()
+print checker.push("localhost","wewewe","#THISISFLAG!")
